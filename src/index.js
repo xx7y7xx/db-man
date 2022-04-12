d1c8286d02af2d40fc1bb9f6a39fa1befc7dc293
Devin Chen <5204106+xxd3vin@users.noreply.github.com>
2022-04-12 19:40:43 +0800

    export more

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 export { default as octokit } from "./octokit";
 export * as githubDb from "./githubDb";
+export * as github from "./github";
+export * as constants from "./constants";
 export { default as Settings } from "./pages/Settings";
