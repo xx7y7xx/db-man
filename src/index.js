7109d3ad18a11435eb9dd55a327c7edfc6c0b0c1
Devin Chen <5204106+xxd3vin@users.noreply.github.com>
2022-05-03 18:52:14 +0800

    remove unused

--- src/index.js
+++ src/index.js
@@ -1,6 +1,2 @@
-export { default as octokit } from "./octokit";
-export * as githubDb from "./githubDb";
-export * as github from "./github";
 export * as constants from "./constants";
-export { default as Settings } from "./pages/Settings";
 export * as utils from "./utils";
