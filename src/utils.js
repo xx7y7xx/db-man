f3bbbab4af841b4f4585d250b56912daf2c80538
Devin Chen <5204106+xxd3vin@users.noreply.github.com>
2022-05-07 21:29:52 +0800

    move funcs

--- src/utils.js
+++ src/utils.js
@@ -20,25 +20,3 @@ export const formatDate = (d) => {
     pad(d.getSeconds())
   );
 };
-
-export const getPrimaryKey = (columns) => {
-  const foundCol = columns.find((col) => col.primary);
-  if (foundCol) {
-    return foundCol.id;
-  }
-
-  console.warn('Primary key not found in columns, fallback to "id"!');
-  return "id";
-};
-
-export const getTablePrimaryKey = (tables, tableName) => {
-  const foundTable = tables.find((table) => table.name === tableName);
-  if (!foundTable) {
-    console.warn(
-      'reference table not found, fallback to use "id" as primaryKey'
-    );
-    return "id";
-  }
-
-  return getPrimaryKey(foundTable.columns);
-};
