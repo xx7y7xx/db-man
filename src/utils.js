93f399562ad9471e62d8a94d1733654a393be0da
devin.chenyang <devin.chenyang@grabtaxi.com>
2023-06-09 22:07:15 +0800

    remove utils

--- src/utils.js
+++ src/utils.js
@@ -1,22 +1,22 @@
-export const uniq = (array) => [...new Set(array)];
+// export const uniq = (array) => [...new Set(array)];
 
-/**
- * @param {Date} d
- * @returns {string} "2021-07-04 01:16:01"
- */
-export const formatDate = (d) => {
-  const pad = (num) => num.toString().padStart(2, "0");
-  return (
-    d.getFullYear() +
-    "-" +
-    pad(d.getMonth() + 1) +
-    "-" +
-    pad(d.getDate()) +
-    " " +
-    pad(d.getHours()) +
-    ":" +
-    pad(d.getMinutes()) +
-    ":" +
-    pad(d.getSeconds())
-  );
-};
+// /**
+//  * @param {Date} d
+//  * @returns {string} "2021-07-04 01:16:01"
+//  */
+// export const formatDate = (d) => {
+//   const pad = (num) => num.toString().padStart(2, "0");
+//   return (
+//     d.getFullYear() +
+//     "-" +
+//     pad(d.getMonth() + 1) +
+//     "-" +
+//     pad(d.getDate()) +
+//     " " +
+//     pad(d.getHours()) +
+//     ":" +
+//     pad(d.getMinutes()) +
+//     ":" +
+//     pad(d.getSeconds())
+//   );
+// };
